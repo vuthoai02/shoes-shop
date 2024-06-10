8b93cf8bf5b0bd1d433d7dc462f969956be16c5a
Vũ Quang Thoại <93929208+vuthoai02@users.noreply.github.com>
2024-06-10 17:48:53 +0700

    Update dbcon.php

--- shoe_store/config/dbcon.php
+++ shoe_store/config/dbcon.php
@@ -1,7 +1,7 @@
 <?php
-$host = "shoes-db.c6wnfmdtgzn0.us-east-1.rds.amazonaws.com";
-$username = "main";
-$password = "password";
+$host = "";
+$username = "";
+$password = "";
 $database = "shoes";
 
 $conn = mysqli_connect($host, $username, $password, $database);
